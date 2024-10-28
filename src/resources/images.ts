de28c208365f4f0c6ef4f2c2063791c3515fcf4b
Jagadees27 <jagadeeswaran2705@gmail.com>
2024-10-28 12:43:11 +0530

    fixed images path

--- src/resources/images.ts
+++ src/resources/images.ts
@@ -1,18 +1,9 @@
+import loginBackground from "../assets/images/login-background.svg";
+import backupPerson from "../assets/images/backup-person.svg";
+import quote from "../assets/images/quote.svg";
+
 export const images = {
-  loginBackground: "/assets/images/login-background.svg",
-  backupPerson: "/assets/images/backup-person.svg",
-  quote: "/assets/images/quote.svg",
-  book1: "/assets/images/book-1.svg",
-  book2: "/assets/images/book-2.svg",
-  book3: "/assets/images/book-3.svg",
-  book4: "/assets/images/book-4.svg",
+  loginBackground,
+  backupPerson,
+  quote,
 };
-export const bookImages = [
-  "/assets/images/book-1.svg",
-  "/assets/images/book-2.svg",
-  "/assets/images/book-3.svg",
-  "/assets/images/book-4.svg",
-  "/assets/images/book-2.svg",
-  "/assets/images/book-3.svg",
-  "/assets/images/book-4.svg",
-];
